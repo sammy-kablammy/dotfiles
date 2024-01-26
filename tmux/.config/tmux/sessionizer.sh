3256f1808ab57a62d61bdfbb961b94b81a0fb579
sammy-kablammy <98327570+sammy-kablammy@users.noreply.github.com>
2024-01-26 09:46:42 -0500

    update sessionizer

--- tmux/.config/tmux/sessionizer.sh
+++ tmux/.config/tmux/sessionizer.sh
@@ -5,9 +5,9 @@ if [[ $# -eq 1 ]]; then
     selected=$1
 else
     # this was the original line, but it seems to hide symlinks? i don't know...
-    # selected=$(find ~/.config ~/my_neovim_plugins -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~ ~/dotfiles ~/my_neovim_plugins ~/notes -mindepth 1 -maxdepth 1 -type d | fzf)
     # this line seems to work better with GNU stow
-    selected=$(find ~ ~/dotfiles ~/my_neovim_plugins ~/notes -mindepth 1 -maxdepth 1 | fzf)
+    # selected=$(find ~ ~/dotfiles ~/my_neovim_plugins ~/notes -mindepth 1 -maxdepth 1 | fzf)
 fi
 
 if [[ -z $selected ]]; then
