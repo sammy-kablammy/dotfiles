e19a78171f4014c94c07b5b59b0536e1d50beb83
sammy-kablammy <98327570+sammy-kablammy@users.noreply.github.com>
2024-03-04 18:18:16 -0500

    disable completion by default. you now need to press ^n to get suggestions.

--- nvim/.config/nvim/lua/plugin-configs/snippets_and_completion.lua
+++ nvim/.config/nvim/lua/plugin-configs/snippets_and_completion.lua
@@ -1,9 +1,25 @@
+-- rough explanation of all the plugins (not entirely accurate but i don't feel
+-- like researching any more so here you go):
+--[[
+- nvim-cmp displays a popup window. gui stuff, basically.
+- cmp_luasnip provides nvim-cmp with a list of snippets
+- luasnip is a source of snippets for nvim-cmp
+- luasnip expands snippets. this modifies the text in the buffer. (you know,
+    the thing you wanted to do the entire time)
+- cmp-nvim-lsp gives you completion suggestions from language servers.
+    you'll notice that if cmp-nvim-lsp is commented out, you don't get lsp stuff
+- friendly-snippets is a collection of snippets that people have made
+--]]
+
 require("luasnip.loaders.from_vscode").lazy_load()
 
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 
 cmp.setup({
+    -- only offer suggestions on keypress; don't have a popup window constantly
+    -- appearing with suggestions.
+    autocompletion = false,
 	window = {
 		documentation = cmp.config.window.bordered(),
 		-- completion = cmp.config.window.bordered(),
@@ -16,7 +32,7 @@ cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		-- (C-n triggers the completion menu - no need to invoke it separately)
 		-- ["<C-y>"] = cmp.mapping.complete(),
-		["<Tab>"] = cmp.mapping.confirm({ select = true }),
+		-- ["<Tab>"] = cmp.mapping.confirm({ select = true }),
 		["<C-y>"] = cmp.mapping.confirm({ select = true }),
 		["<C-e>"] = cmp.mapping.abort(),
 		["<C-u>"] = cmp.mapping.scroll_docs(-4),
@@ -26,14 +42,7 @@ cmp.setup({
 		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
 	}, {
+		{ name = "path" },
 		{ name = "buffer" },
 	}),
 })
-
--- partial snippet example
--- local ls = require('luasnip')
--- local s = ls.snippet
--- local t = ls.text_node
--- ls.add_snippets('lua', {
---     s('sample', t('-- this is a sample snippet'))
--- })
