210dac83571860c5fc6c4c3533f76d86b820e5e8
sammy-kablammy <98327570+sammy-kablammy@users.noreply.github.com>
2025-01-05 22:08:44 -0500

    update python ftplugin

--- nvim/.config/nvim/after/ftplugin/python.lua
+++ nvim/.config/nvim/after/ftplugin/python.lua
@@ -2,4 +2,7 @@ vim.keymap.set("n", "<leader><enter>", "<cmd>!python3 %<cr>")
 
 InsertMap("p", 'print("")<left><left>')
 
-vim.b.sam_documentation_url = "https://docs.python.org/3/"
+vim.b.sam_documentation_url = "https://docs.python.org/3/library/index.html"
+
+-- Install with 'pip install black', don't bother with Mason
+vim.bo.formatprg = "black - --quiet"
