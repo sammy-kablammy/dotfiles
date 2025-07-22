c0e607d12afc59cd14a6ea86783407ec75d1df9d
sammy-kablammy <98327570+sammy-kablammy@users.noreply.github.com>
2025-07-22 18:08:49 -0400

    make install_font script actually work

--- install_font.sh
+++ install_font.sh
@@ -1,10 +1,2 @@
-```bash
-echo i have not thoroughly tested this script
-echo i have not thoroughly tested this script
-echo i have not thoroughly tested this script
-curl --output-dir ~/.local/share/fonts/truetype/ -LO \
-https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/NoLigatures/Regular/JetBrainsMonoNLNerdFontMono-Regular.ttf
-echo i have not thoroughly tested this script
-echo i have not thoroughly tested this script
-echo i have not thoroughly tested this script
-```
+curl --create-dirs --output-dir ~/.local/share/fonts/truetype/ -LO \
+"https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/NoLigatures/Regular/JetBrainsMonoNLNerdFontMono-Regular.ttf"
